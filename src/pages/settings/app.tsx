f38af5bf79f47bf37074d7fd0a2a810bd4015ae2
Kevin <kevinadikasaputragithub@gmail.com>
2026-03-04 19:57:14 +0700

    :wrench: Chore: create new page /settings/user and /settings/app

---
+++ src/pages/settings/app.tsx
@@ -0,0 +1,13 @@
+import Container from "@/common/components/elements/Container"
+
+const app = () => {
+  return (
+    <Container data-aos="fade-up">
+      <div>
+        <h1>Settings</h1>
+      </div>
+    </Container>
+  )
+}
+
+export default app
---
+++ src/pages/settings/user.tsx
@@ -0,0 +1,13 @@
+import Container from "@/common/components/elements/Container"
+
+const user = () => {
+  return (
+    <Container data-aos="fade-up">
+      <div>
+        <h1>user</h1>
+      </div>
+    </Container>
+  );
+}
+
+export default user
