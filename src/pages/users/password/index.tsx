ae4e9bb5deab21a1bfa74347bf8fa17309e696ca
Kevin <kevinadikasaputragithub@gmail.com>
2026-03-04 20:04:33 +0700

    :wrench: Chore: create deep routing /users/password/

---
+++ src/pages/users/password/index.tsx
@@ -0,0 +1,13 @@
+import Container from "@/common/components/elements/Container"
+
+const password = () => {
+  return (
+    <Container data-aos="fade-up">
+      <div>
+        <h1> user password</h1>
+      </div>
+    </Container>
+  )
+}
+
+export default password
