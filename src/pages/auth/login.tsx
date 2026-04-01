0bad1e0522ceb9abe6da192c51bdfd27e64151cf
Kevin <kevinadikasaputragithub@gmail.com>
2026-04-01 08:50:49 +0700

    :art: Style: styling login pages

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -11,18 +11,21 @@ const HalamanLogin = () => {
   };
 
   return (
-    <div style={{ padding: "20px", maxWidth: "400px", margin: "0 auto" }}>
-      <h1>Halaman Login</h1>
-
-      <div style={{ marginBottom: "20px" }}>
+    <div className="mx-auto p-10 bg-white m-10 rounded-2xl">
+      <h1 className="text-black m-4 text-center">Halaman Login</h1>
+      <div className="flex justify-center m-4 flex-col gap-10">
+        <div className="flex flex-col gap-4">
+          <input type="text" placeholder="Masukkan username" className="border-b-2 border-gray-300 p-2 rounded-lg text-black" />
+          <input type="password" placeholder="Masukkan Password" className="border-b-2 border-gray-300 p-2 rounded-lg text-black" />
+        </div>
         <Button
           onClick={handleLogin}
         >
-          Login (Navigasi ke Product)
+          Login to Products
         </Button>
       </div>
 
-      <p style={{ textAlign: "center" }}>
+      <p className="text-black">
         Belum punya akun? {" "}
         <Link href="/auth/register" style={{ color: "#0070f3", textDecoration: "underline" }}>
           Register di sini
